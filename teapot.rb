f73a26d9acc28af030add2d668b75063b22294b2
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-06-27 05:11:35 +1200

    More complex build targets make it easier to run Kai.

--- teapot.rb
+++ teapot.rb
@@ -10,10 +10,43 @@ define_target "kai" do |target|
 		build_directory(package.path, 'source', environment)
 	end
 	
+	target.depends :platform
+	
 	target.depends "Language/C++11"
 	target.depends "Library/llvm-engine"
 	
-	target.provides "Library/kai"
+	target.provides "Library/Kai" do
+		ldflags ["-lKai"]
+	end
+end
+
+define_target "kai-interpreter" do |target|
+	target.depends "Library/Kai"
+	
+	target.run do |environment|
+		run_executable("bin/kai", environment)
+	end
+	
+	target.provides "Interpreter/Kai"
+end
+
+define_target "kai-tests" do |target|
+	target.build do |environment|
+		build_directory(package.path, 'test', environment)
+	end
+	
+	target.run do |environment|
+		environment = environment.flatten
+		
+		Commands.run(environment[:install_prefix] + "bin/kai-test-runner")
+	end
+	
+	target.depends :platform
+	target.depends "Language/C++11"
+	target.depends "Library/UnitTest"
+	target.depends "Library/Kai"
+	
+	target.provides "Test/Kai"
 end
 
 
@@ -21,5 +54,7 @@ define_configuration "kai" do |configuration|
 	configuration[:source] = "https://github.com/dream-framework/"
 	
 	configuration.import! "platforms"
+	
+	configuration.require "unit-test"
 	configuration.require "llvm"
 end
