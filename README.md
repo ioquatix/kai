fe591113adebbece4d91a63a2854c6643be4b47b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-08-26 02:24:50 +1200

    Update project and use llvm 4.0+

--- README.md
+++ README.md
@@ -7,18 +7,27 @@ It has a number of useful features:
 * Arbitrary precision unsigned and fixed-point math by default.
 * Easy to embed and extend into other C++ projects.
 
-For documentation and examples, please see the main [project page][1].
+## Setup
 
-[1]: http://www.oriontransfer.co.nz/research/kai
+The build tool `teapot` needs to be installed (which requires [Ruby][2]):
 
-## Installation
+	$ gem install teapot
 
-Ensure that you already have a working install of Ruby 1.9.3+
+[2]: http://www.ruby-lang.org/en/downloads/
 
-    $ gem install teapot
+### Install Dependencies
+
+To fetch project dependencies, run the following:
+
 	$ cd kai
 	$ teapot fetch
-	$ teapot build Library/kai
+
+### Validate Behaviour
+
+The project can be checked by running unit tests:
+
+	$ cd kai
+	$ teapot Test/Kai
 
 ## Usage
 
@@ -32,29 +41,37 @@ This provides a standard REPL interface.
 
 ## Implementation
 
-### Object Model ###
+### Object Model
 
 Kai has a context-dependent object model which means that objects can be transparently moved between instances. Due to the indirection, there is a small performance hit due to the context-dependent lookup.
 
-### Memory Model ###
+### Memory Model
 
 Kai has a precise mark and sweep garbage collection with well-defined check points. The garbage collection is combined with a basic linked-list memory manager which includes a linked list of free allocations. The object allocator is designed for small object allocations between 32 and 256 bytes. It can handle larger objects but the performance will not be as good.
 
-### Interpreter Model ###
+### Interpreter Model
 
 The Kai stack frame serves as the fundamental unit of evaluation and lookup. Due to this, the interpreter context is stored entirely within the root stack frame from which futher evaluation occurs. Almost all Kai functions take the current stack frame as an argument.
 
 Due to this, supporting multiple isolated interpreters is as easy as having separate stack frames. Isolated interpreters can be run on different threads and will not interact in any way (except by design).
 
-Compatibility
--------------
+#### Compatibility
 
-Kai will build and work on any platform that provides a modern C++11 compiler, e.g. Ubuntu Precise+, Mac OS X 10.6+. Kai is primarily built and tested with `clang++`.
+Kai will build and work on any platform that provides a modern C++14 compiler, e.g. Ubuntu Precise+, Mac OS X 10.6+. Kai is primarily built and tested with `clang++`.
 
-License
--------
+## Contributing
 
-Copyright (c) 2006, 2011 Samuel G. D. Williams. <http://www.oriontransfer.co.nz>
+1. Fork it.
+2. Create your feature branch (`git checkout -b my-new-feature`).
+3. Commit your changes (`git commit -am 'Add some feature'`).
+4. Push to the branch (`git push origin my-new-feature`).
+5. Create new Pull Request.
+
+## License
+
+Released under the MIT license.
+
+Copyright, 2017, by Samuel Williams. All rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- teapot.rb
+++ teapot.rb
@@ -3,71 +3,118 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-teapot_version "1.0.0"
+teapot_version "2.0"
 
-define_target "kai" do |target|
+# Project Metadata
+
+define_project "kai" do |project|
+	project.title = "Kai"
+	project.summary = 'A brief one line summary of the project.'
+	
+	project.license = 'MIT License'
+	
+	project.add_author 'Samuel Williams', email: 'samuel.williams@oriontransfer.co.nz'
+	# project.website = 'http://Kai.com/'
+	
+	project.version = '0.1.0'
+end
+
+# Build Targets
+
+define_target "kai-library" do |target|
 	target.build do
 		source_root = target.package.path + 'source'
 		
 		copy headers: source_root.glob('Kai/**/*.{h,hpp}')
 		
-		build static_library: "Kai",
-			source_files: source_root.glob('Kai/**/*.cpp')
+		build static_library: 'Kai', source_files: source_root.glob('Kai/**/*.cpp')
 	end
 	
 	target.depends "Build/Files"
 	target.depends "Build/Clang"
-
-	target.depends :platform
-	target.depends "Language/C++11"
 	
-	target.depends "Library/dynamic-linker"
-	#target.depends "Library/llvm-engine"
+	target.depends :platform
+	target.depends 'Language/C++14', private: true
+	
+	# target.depends "Library/dynamic-linker"
+	target.depends "Library/llvm-engine"
 	
 	target.provides "Library/Kai" do
-		ldflags ["-lKai"]
+		append linkflags [
+			->{install_prefix + 'lib/libKai.a'},
+		]
 	end
-end
-
-define_target "kai-interpreter" do |target|
-	target.depends "Library/Kai"
-	
-	target.build do
-		source_root = target.package.path + 'source'
-
-		build executable: "kai",
-			source_files: source_root.glob('Kai-interpreter/**/*.cpp')
-	end
-	
-	target.provides "Executable/Kai"
 end
 
 define_target "kai-tests" do |target|
-	target.build do
+	target.build do |*arguments|
 		test_root = target.package.path + 'test'
 		
-		run tests: 'Kai', source_files: test_root.glob("Kai/**/*.cpp")
+		run tests: 'Kai', source_files: test_root.glob('Kai/**/*.cpp'), arguments: arguments
 	end
 	
 	target.depends :platform
-	target.depends "Language/C++11"
+	target.depends "Language/C++14"
 	target.depends "Library/UnitTest"
 	target.depends "Library/Kai"
 	
 	target.provides "Test/Kai"
 end
 
+define_target "kai-executable" do |target|
+	target.build do
+		source_root = target.package.path + 'source'
 
-define_configuration "kai" do |configuration|
-	configuration[:source] = "https://github.com/dream-framework/"
+		build executable: "Kai",
+			source_files: source_root.glob('Kai-interpreter/**/*.cpp')
+	end
 	
+	target.depends "Language/C++14", private: true
+	
+	target.depends "Library/Kai"
+	
+	target.provides "Executable/Kai"
+end
+
+define_target "kai-run" do |target|
+	target.build do |*arguments|
+		run executable: "Kai", arguments: arguments
+	end
+	
+	target.depends "Executable/Kai"
+	
+	target.provides "Run/Kai"
+end
+
+# Configurations
+
+define_configuration 'development' do |configuration|
+	configuration[:source] = "https://github.com/kurocha"
+	
+	configuration.import 'kai'
+	
+	# Provides all the build related infrastructure:
 	configuration.require "platforms"
 	
 	host /linux/ do
 		configuration.require "linux-dynamic-linker"
 	end
 	
+	# Provides unit testing infrastructure and generators:
+	configuration.require 'unit-test'
+	
+	# Provides some useful C++ generators:
+	configuration.require 'generate-cpp-class'
+	
+	configuration.require "generate-project"
+	configuration.require "generate-travis"
+	
 	configuration.require "build-files"
 	configuration.require "unit-test"
+end
+
+define_configuration 'kai' do |configuration|
+	configuration.public!
+	
 	configuration.require "llvm"
 end
